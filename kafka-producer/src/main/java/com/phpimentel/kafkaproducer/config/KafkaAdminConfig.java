c34b2512a9b1076b6b8a23efd4961d85c3505b7f
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-08-22 17:11:18 -0300

    Configurando KafkaAdmin

---
+++ kafka-producer/src/main/java/com/phpimentel/kafkaproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,33 @@
+package com.phpimentel.kafkaproducer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    public final KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopics topics() {
+        return new NewTopics(
+                TopicBuilder.name("str-topic").partitions(2).replicas(1).build()
+        );
+    }
+}
