078a171120e0c47af2b5b266410472233a39ca31
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-08-22 18:55:45 -0300

    Adicionando função de Callback

--- kafka-producer/src/main/java/com/phpimentel/kafkaproducer/services/ProducerService.java
+++ kafka-producer/src/main/java/com/phpimentel/kafkaproducer/services/ProducerService.java
@@ -1,9 +1,13 @@
 package com.phpimentel.kafkaproducer.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
+@Slf4j
 @Service
 public class ProducerService {
 
@@ -11,6 +15,15 @@ public class ProducerService {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        this.kafkaTemplate.send("str-topic", message);
+        this.kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if (Objects.nonNull(success)) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {}", success.getRecordMetadata().partition());
+                        log.info("Offset {}", success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 }
