cd4fb40f6fc5f110e75a8029045c104eee4e3173
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-08-23 13:26:20 -0300

    Adicionando implementação payment-topic KafkaAdmin

---
+++ payment-service/src/main/java/com/phpimentel/paymentservice/configs/KafkaAdminConfig.java
@@ -0,0 +1,33 @@
+package com.phpimentel.paymentservice.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopics newTopics() {
+        return new NewTopics(
+                TopicBuilder.name("payment-topic").partitions(1).build()
+        );
+    }
+}
