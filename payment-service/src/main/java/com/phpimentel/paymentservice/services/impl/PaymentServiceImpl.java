e5b8d149371223481d8a523db387f28b6ff29a3b
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-08-23 13:50:48 -0300

    Adicionando configuração do produtor para envio de objetos json

--- payment-service/src/main/java/com/phpimentel/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/phpimentel/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.phpimentel.paymentservice.services.impl;
 
 import com.phpimentel.paymentservice.models.Payment;
 import com.phpimentel.paymentservice.services.PaymentService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Slf4j
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    @Autowired
+    private KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
         log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        this.kafkaTemplate.send("payment-topic", payment);
     }
 }
