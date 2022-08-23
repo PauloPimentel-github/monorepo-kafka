98bb8b1965e17e90599b6a0a2a2c9f7bc4ba40ec
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-08-22 21:57:36 -0300

    Configurando, determinando partição para um Consumer específico

--- kafka-consumer/src/main/java/com/phpimentel/kafkaconsumer/listeners/ConsumerListener.java
+++ kafka-consumer/src/main/java/com/phpimentel/kafkaconsumer/listeners/ConsumerListener.java
@@ -2,18 +2,27 @@ package com.phpimentel.kafkaconsumer.listeners;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class ConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "containerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"0"})
+            },
+            containerFactory = "containerFactory")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "containerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"1"})
+            },
+            containerFactory = "containerFactory")
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
     }
